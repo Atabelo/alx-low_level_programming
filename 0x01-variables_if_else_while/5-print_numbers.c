780dc936bf6631f5ccb7a3ef3a78b35da8b63ae2
Jesuelo Atabeh <jesueloatabeh@gmail.com>
2022-09-09 14:43:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print number from 0 to 9
+ * Description: 'Output the size of dataTypes'
+ *Return: Always 0
+ */
+
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+printf("%d", n++);
+
+
+printf("\n");
+return (0);
+}

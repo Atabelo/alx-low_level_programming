04afc017bd4398953285fa0be37382a6f02b9805
Jesuelo Atabeh <jesueloatabeh@gmail.com>
2022-09-14 13:44:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - print putchar
+ * main - Entry point.
  * Description: write a program that prints "_putchar" followed by a new line.
  * Return: Always 0 (Success.)
  */

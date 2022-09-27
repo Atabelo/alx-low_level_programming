b69df5df68b8954219648437d787fbd4ae1482c3
Jesuelo Atabeh <jesueloatabeh@gmail.com>
2022-09-26 19:52:36 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,10 +6,10 @@
  * Return: pointer to destination memory area
  */
 
- char *_memcpy(char *dest, char *src, unsigned int n)
- {
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	return (dest);

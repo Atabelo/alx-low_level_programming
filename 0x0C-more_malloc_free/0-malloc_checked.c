55c0f9c916c10cf1dfa7cebae9acbf232481ae31
Jesuelo Atabeh <jesueloatabeh@gmail.com>
2022-10-06 10:03:06 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * malloc_checked - array for prints a string
- * @b: number of memory
- * Return: void
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
  */
-
 void *malloc_checked(unsigned int b);
 {
 void *p;
 
 p = malloc(b);
 if (p == NULL)
-{
 exit(98);
-}
-
 return (p);
 }
